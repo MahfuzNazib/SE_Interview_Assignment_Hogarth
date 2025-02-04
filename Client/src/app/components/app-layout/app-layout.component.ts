f2de151a06008cf93f1b50fcd9e3432c340876de
NazibMahfuz <mahfuz@uniclienttechnologies.com>
2025-02-04 08:24:14 +0600

    UI - User List Component Design

--- Client/src/app/components/app-layout/app-layout.component.ts
+++ Client/src/app/components/app-layout/app-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-app-layout',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, UserComponent],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.css'
 })
